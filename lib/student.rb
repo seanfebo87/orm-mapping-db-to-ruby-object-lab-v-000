e8f32a158a64f92bd14c51267a68bb9fadaea1fb
Sean Febo <seanfebo@techie.com>
2018-05-03 06:50:13 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,6 +23,12 @@ class Student
     end.first
   end
   
+  def count_all_students_in_grade_9
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = 9 
+    SQL
+    DB[:conn].execute(sql)
+  end
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade) 
