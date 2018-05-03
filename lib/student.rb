bc2d8396cac39c63b406b7711472ab7559917961
Sean Febo <seanfebo@techie.com>
2018-05-03 06:57:59 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,7 +29,7 @@ class Student
     SQL
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
-    end.first
+    end
   end
   
   def self.students_below_12th_grade
@@ -38,7 +38,7 @@ class Student
     SQL
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
-    end.first
+    end
   end
   def save
     sql = <<-SQL
