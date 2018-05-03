47527c11e65aa3f57a9c24eeb7e188993f7b8814
Sean Febo <seanfebo@techie.com>
2018-05-03 06:57:30 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,7 +37,7 @@ class Student
       SELECT * FROM students WHERE grade < 12  
     SQL
     DB[:conn].execute(sql).map do |row|
-      self,new_from_db(row)
+      self.new_from_db(row)
     end.first
   end
   def save
